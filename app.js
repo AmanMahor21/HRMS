3f1f63d62d2fef968a9005e673592401e16fcf0d
Aman <amanmahor021@gmail.com>
2025-05-04 11:41:20 +0530

    add login api

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 const app = express();
 
+
 app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
 
 app.use("/api/auth", authRoutes);
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -2,12 +2,16 @@
 
 import Candidate from '../modals/Candidate.js';
 import User from '../modals/User.js';
+import bcrypt from 'bcrypt';
 
 export const register = async (req, res) => {
     try {
         const { full_name, email, password, } = req.body;
 
         console.log(full_name, email, password, 'userr');
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         // Check if candidate with email already exists
         const existingCandidate = await Candidate.findOne({ email });
         if (existingCandidate) {
@@ -19,7 +23,7 @@ export const register = async (req, res) => {
         const user = new User({
             full_name,
             email,
-            password,
+            password: hashedPassword,
         });
 
         await user.save();
@@ -33,3 +37,29 @@ export const register = async (req, res) => {
         res.status(500).json({ message: 'Server Error', error: error.message });
     }
 };
+
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Email not found, please register' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Password does not match' });
+        }
+
+        res.status(200).json({
+            message: 'Login successful',
+            user,
+        });
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
